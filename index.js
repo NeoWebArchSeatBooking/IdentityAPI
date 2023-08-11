0bd43af95180d212d867c01cf29a32d0d567503f
DhamoSG <sgd.daran@gmail.com>
2023-08-11 15:58:27 +0000

    refactored REST API

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json')
 const { fetchUserWithToken, verifyToken } = require("./idp.authenticator");
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-app.get("/idp/verify", verifyToken);
-app.get("/idp/fetch", fetchUserWithToken);
+app.use('/v1/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.get("/v1/idp/status", verifyToken);
+app.get("/v1/idp/fetch", fetchUserWithToken);
 
 const port = process.env.PORT || 3000;
 
